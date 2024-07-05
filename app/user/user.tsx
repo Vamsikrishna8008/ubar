9cac1dd1465b2ff9e71bfe7a562190ddfb12248d
Vamsikrishna8008 <n.vamsi8008@gmail.com>
2024-07-05 20:45:14 +0530

    intial commit

---
+++ app/user/user.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const user = () => {
+  return (
+    <View style={{paddingVertical:30}}>
+      <Text style={{color:"white"}}>user</Text>
+    </View>
+  );
+};
+
+export default user;
+
+const styles = StyleSheet.create({});
